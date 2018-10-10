40f4be80336af60962a7cd6b3a498ea183750190
Jaynit Apex <jaynitapex@gmail.com>
2018-10-10 18:28:53 +0530

    design complete withou header and tabbar

--- App.js
+++ App.js
@@ -1,18 +1,77 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, StatusBar, Image, TouchableOpacity, Button, ScrollView} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import { Constants, LinearGradient} from 'expo';
+import { AreaChart, Grid } from 'react-native-svg-charts';
+import { PieChart } from 'react-native-svg-charts';
+import ModalDropdown from 'react-native-modal-dropdown';
+import TabNavigator from 'react-native-tab-navigator';
+
+import * as shape from 'd3-shape'
+
 import styles from "./styles";
 
 export default class App extends React.Component {
+  static  defaultProps = {
+          transactionData : [
+            {
+              date: "23 May, 2018",
+              name: "Starbucks",
+              image: require("./assets/IOTA.png"),
+              cat: "Coffee",
+              coin: "4 IOTA"
+            },
+            {
+              date: "8 june, 2018",
+              name: "Bitcoin",
+              image: require("./assets/ethereum.png"),
+              cat: "Investment",
+              coin: "12 BTC"
+            },
+            {
+              date: "13 Aug, 2018",
+              name: "Burger",
+              image: require("./assets/ethereum.png"),
+              cat: "Food",
+              coin: "5 ETH"
+            }
+          ],
+          month : [
+            "Feb",
+            "Mar",
+            "Apr",
+            "May"
+          ]
+
+    }
+
   render() {
+    const data = [ 40.2, 19.6, 13.2, 16.9, 15.4]
+
+    const randomColor = () => ('#' + (Math.random() * 0xFFFFFF << 0).toString(16) + '000000').slice(0, 7)
+
+    const pieData = data.filter(value => value > 0).map((value, index) => ({
+            value,
+            svg: {
+                fill: randomColor(),
+                onPress: () => console.log('press', index),
+            },
+            key: `pie-${index}`,
+        }))
+
     return (
-      <ScrollView>
-        <View style={styles.container}>
+      <View style={styles.container}>
+        <ScrollView>
           <View style={styles.statusBar}></View>
 
           <View style= {styles.searchBarRow}>
-            <TextInput underlineColorAndroid="transparent" style={styles.searchInputText}/>
+            <TextInput
+              underlineColorAndroid="transparent"
+              style={styles.searchInputText}
+              placeholder="Start typing to search ..."
+              placeholderTextColor="#5B7293"
+            />
             <Image style={styles.profileImage} source={require('./assets/sara.jpg')}/>
             <Ionicons name="ios-notifications-outline" size={55} style={styles.notificationIcon}/>
           </View>
@@ -30,7 +89,7 @@ export default class App extends React.Component {
                     <Ionicons name="ios-trending-up" size={30} style={styles.portStyle}/>
                  </View>
                  <View style= {styles.commonIcon}>
-                    <Ionicons name="ios-add" size={30} style= {styles.commonTextColor}/>
+                    <Ionicons name="ios-add" size={23} style= {styles.commonPortTextColor}/>
                     <Text style={styles.portPercentage}> 29% </Text>
                  </View>
                 </LinearGradient>
@@ -42,10 +101,10 @@ export default class App extends React.Component {
                     <Ionicons name="ios-trending-down" size={30} style={styles.ioniconsDown}/>
                  </View>
                  <View style= {styles.commonIcon}>
-                    <Ionicons name="ios-remove" size={10} style= {styles.removeIconColor}/>
+                    <Ionicons name="ios-remove" size={15} style= {styles.removeIconColor}/>
                     <Text style={styles.percentage}> 5% </Text>
                  </View>
-                </TouchableOpacity>
+              </TouchableOpacity>
             </View>
 
             <View style={{flexDirection: 'row'}}>
@@ -55,7 +114,7 @@ export default class App extends React.Component {
                       <Ionicons name="ios-trending-up" size={30} style={styles.ioniconsUp}/>
                    </View>
                    <View style= {styles.commonIcon}>
-                      <Ionicons name="ios-add" size={30} style= {styles.commonBitcoin}/>
+                      <Ionicons name="ios-add" size={23} style= {styles.commonBitcoin}/>
                       <Text style={styles.bitCoinPercentage}> 39% </Text>
                    </View>
                 </TouchableOpacity>
@@ -63,10 +122,10 @@ export default class App extends React.Component {
                <TouchableOpacity style={styles.buttonGradient}>
                    <View style= {styles.commonBtnStyle}>
                       <Text style= {styles.coinDownName}> Crypterium </Text>
-                      <Ionicons name="ios-trending-down" size={30} style={styles.ioniconsDown}/>
+                      <Ionicons name="ios-trending-down" size={23} style={styles.ioniconsDown}/>
                    </View>
                    <View style= {styles.commonIcon}>
-                      <Ionicons name="ios-remove" size={30} style= {styles.removeIconColor}/>
+                      <Ionicons name="ios-remove" size={15} style= {styles.removeIconColor}/>
                       <Text style={styles.percentage}> 5% </Text>
                    </View>
                 </TouchableOpacity>
@@ -74,40 +133,258 @@ export default class App extends React.Component {
           </View>
 
           <TouchableOpacity style= {styles.addWalletbtn}>
-            <View style={{flexDirection: 'row',justifyContent: 'center', marginTop: 30}}>
+            <View style={{flexDirection: 'row',justifyContent: 'center', alignItem: 'center'}}>
               <Ionicons name="ios-add-circle-outline" size={15} style={styles.addWalletIcon}/>
-              <Text style={styles.addWalletTextColor}>Add Wallet </Text>
+              <Text style={styles.addWalletTextColor}> Add Wallet </Text>
             </View>
           </TouchableOpacity>
 
           <View style= {styles.portBalanceStyle}>
+             <View style= {styles.commonBtnStyle}>
+                <Text style= {styles.yourPortBalanceBtnStyle}> Your Portfolio Balance </Text>
+             </View>
+             <View style= {styles.portBlancePercentageIcon}>
+                <View style= {{flexDirection: 'row'}}>
+                  <Text style= {styles.dollarIconColor}> $ </Text>
+                  <Text style={styles.portBalance}> 397,900 </Text>
+                </View>
+                <View style= {{flexDirection: 'row'}}>
+                  <Text style={styles.balancePercentage}> 15% </Text>
+                  <Ionicons name="ios-trending-up" size={30} style= {styles.portBalanceUpArrow}/>
 
+                </View>
+             </View>
           </View>
-          <View style= {{flex: 1, flexDirection: 'row', marginTop: 20}}>
-              <TouchableOpacity style={styles.buttonGradient}>
-                <LinearGradient
+
+          <View style= {{flex: 1, flexDirection: 'row', marginTop: 10}}>
+           <TouchableOpacity style={styles.buttonGradientBalance}>
+               <View style= {styles.depositeBtnStyle}>
+                  <Text style= {styles.dipositeMoney}> Deposit Money </Text>
+               </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonGradientBalance}>
+              <LinearGradient
                    colors={['#8ADE8E', '#56C3A4']}
                    start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}
                    style={{borderRadius: 10}}
                  >
-                 <View style= {styles.commonBtnStyle}>
-                    <Text style= {styles.commonTextColor}> Your Portfolio </Text>
-                    <Ionicons name="ios-trending-up" size={30} style={styles.portStyle}/>
-                 </View>
-                 <View style= {styles.commonIcon}>
-                    <Ionicons name="ios-add" size={30} style= {styles.commonTextColor}/>
-                    <Text style={styles.portPercentage}> 29% </Text>
-                 </View>
-                </LinearGradient>
-               </TouchableOpacity>
-            <TouchableOpacity style= {styles.withdrawBtn}>
-              <View style= {{flexDirection: 'row'}}>
-                  <Text>Withdraw</Text>
+               <View style= {styles.depositeBtnStyle}>
+                  <Text style= {styles.withdraw}> Withdraw </Text>
+               </View>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+
+          <View style= {styles.balanceHistory}>
+            <View style= {styles.commonBtnStyle}>
+              <Text style= {styles.yourPortBalanceBtnStyle}> Balance History </Text>
+              <Text style= {styles.showBalanceHistoryStyle}> Show Period: Last 30 days </Text>
+            </View>
+            <View>
+               <AreaChart
+                    style={{ height: 200 }}
+                    data={ data }
+                    contentInset={{ top: 30, bottom: 30 }}
+                    curve={ shape.curveNatural }
+                    svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+                >
+                <Grid/>
+              </AreaChart>
+            </View>
+            <View style= {styles.balanceHisrtoryMonthStyle}>
+              {
+                this.props.month.map((data, index) => {
+                  return(
+                    <Text style={{color: '#90A1B7'}}> {data} </Text>
+                  );
+                })
+              }
+            </View>
+          </View>
+
+          <View style= {styles.portfolioDistribution}>
+            <View style= {styles.commonBtnStyle}>
+              <Text style= {styles.yourPortBalanceBtnStyle}> Portfolio Distribution </Text>
+            </View>
+            <View>
+               <PieChart
+                  style={ { height: 200 } }
+                  data={ pieData }
+                />
+            </View>
+            <View style={styles.portfolioDisatribution}>
+              <View style={{flexDirection: 'row'}}>
+                  <Ionicons name="ios-radio-button-off" size={20} style={{marginRight: 10, color: randomColor() }}/>
+                  <Text>40.2 BCC, 38.17%</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                  <Ionicons name="ios-radio-button-off" size={20} style={{marginRight: 10, color: randomColor()}}/>
+                  <Text>16.9 LTC, 16.04%</Text>
+              </View>
+            </View>
+            <View style={styles.portfolioDisatribution}>
+              <View style={{flexDirection: 'row'}}>
+                  <Ionicons name="ios-radio-button-off" size={20} style={{marginRight: 10, color: randomColor()}}/>
+                  <Text>19.6 BTC, 18.61%</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                  <Ionicons name="ios-radio-button-off" size={20} style={{marginRight: 10, color: randomColor()}}/>
+                  <Text>15.4 ETH, 14.62%</Text>
+              </View>
+            </View>
+            <View style={{flexDirection: 'row', marginTop: 10, padding: 10}}>
+                <Ionicons name="ios-radio-button-off" size={20} style={{marginRight: 10, color: randomColor()}}/>
+                <Text>13.2 IOTA, 12.53%</Text>
+            </View>
+          </View>
+
+          <View style= {{flexDirection: 'row'}}>
+            <TouchableOpacity style= {styles.addWallet2nbtn}>
+              <View style={{flexDirection: 'row',justifyContent: 'center', marginTop: 30}}>
+                <Ionicons name="ios-add-circle-outline" size={20}/>
+                <Text> Add Wallet </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.bitcoinBTCbtn}>
+              <View style={styles.bitcoinsBTC}>
+                <Image style={styles.profileImage} source={require('./assets/bitcoin.png')}/>
+                <View style={{flex: 1}}>
+                   <Text style={styles.coinNameColor}> Bitcoins BTC </Text>
+                   <Text style={{fontSize: 25}}> 19.6 </Text>
+                </View>
               </View>
             </TouchableOpacity>
           </View>
+
+          <View style= {{flexDirection: 'row'}}>
+            <TouchableOpacity style={styles.bitcoinBTCbtn}>
+              <View style={styles.bitcoinsBTC}>
+                <Image style={styles.profileImage} source={require('./assets/litecoin.png')}/>
+                <View style={{flex: 1}}>
+                   <Text style={styles.coinNameColor}> Litecoin LTC </Text>
+                   <Text style={{fontSize: 25}}> 16.9 </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.bitcoinBTCbtn}>
+              <View style={styles.bitcoinsBTC}>
+                <Image style={styles.profileImage} source={require('./assets/IOTA.png')}/>
+                <View style={{flex: 1}}>
+                   <Text style={styles.coinNameColor}> IOTA </Text>
+                   <Text style={{fontSize: 25}}> 13.2 </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style= {{flexDirection: 'row'}}>
+            <TouchableOpacity style={styles.bitcoinBTCbtn}>
+              <View style={styles.bitcoinsBTC}>
+                <Image style={styles.profileImage} source={require('./assets/ethereum.png')}/>
+                <View style={{flex: 1}}>
+                   <Text style={styles.coinNameColor}> Ethereum ETH </Text>
+                   <Text style={{fontSize: 25}}> 15.4 </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.bitcoinBTCbtn}>
+              <View style={styles.bitcoinsBTC}>
+                <Image style={styles.profileImage} source={require('./assets/bitconnect.png')}/>
+                <View style={{flex: 1}}>
+                   <Text style={styles.coinNameColor}> Bitconnect BCC </Text>
+                   <Text style={{fontSize: 25}}> 40.2 </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={{flex: 1, flexDirection: 'row', marginVertical: 20, padding: 10, justifyContent: 'space-between'}}>
+            <Text style={{fontSize: 20,color: '#90A1B7'}}> Sent Money To  </Text>
+          </View>
+
+          <View style= {{flex: 1, flexDirection: 'row'}}>
+            <ScrollView horizontal={true}>
+              <View style={styles.sentMoneyToView}>
+                  <Image style={styles.profileSentMoneyImage} source={require('./assets/user1.jpeg')}/>
+                <Text> Genie Brawner </Text>
+              </View>
+              <View style={styles.sentMoneyToView}>
+                  <Image style={styles.profileSentMoneyImage} source={require('./assets/user2.jpg')}/>
+                <Text> Zina Triano </Text>
+              </View>
+              <View style={styles.sentMoneyToView}>
+                  <Image style={styles.profileSentMoneyImage} source={require('./assets/user3.jpg')}/>
+                <Text> Gayhart </Text>
+              </View>
+              <View style={styles.sentMoneyToView}>
+                  <Image style={styles.profileSentMoneyImage} source={require('./assets/user1.jpeg')}/>
+                <Text> John Doe </Text>
+              </View>
+            </ScrollView>
+              <View style={styles.addSentMoneyPerson}>
+                <Ionicons name="ios-add-circle-outline" size={50} style={{color:'#0FB9A5'}} />
+                <Text style={{color:'#00E7CD'}}>Add More</Text>
+              </View>
+          </View>
+
+
+          <View style={{flex: 1, flexDirection: 'row', marginVertical: 20, padding: 10, justifyContent: 'space-between'}}>
+            <Text style={{fontSize: 20,color: '#90A1B7'}}> Transactions </Text>
+            <Text style={{fontSize: 15,marginTop: 5}}>Order By: Last 30 days </Text>
+          </View>
+
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <ScrollView horizontal={true}>
+            {
+              this.props.transactionData.map((data, index) => {
+                  return(
+                    <TouchableOpacity style={styles.transactionsStyle}>
+                      <Text style={{fontSize: 15}}>{data.date}</Text>
+                      <View style={styles.transactionView}>
+                        <Image style={styles.transactionPortImage} source={data.image}/>
+                        <View style={{flex: 1}}>
+                           <Text style={styles.transactionMainName}> {data.name} </Text>
+                        </View>
+                      </View>
+                      <Text style={styles.transactionCatName}>{data.cat}</Text>
+                      {data.name == 'Bitcoin' ? <Text style={{textAlign: 'right', fontSize: 18, color: '#44DA79'}}>{data.coin}</Text>
+                                              : <Text style={{textAlign: 'right', fontSize: 18}}>{data.coin}</Text>}
+                    </TouchableOpacity>
+                  );
+              })
+            }
+            </ScrollView>
+          </View>
+        </ScrollView>
+        <TabNavigator>
+            <TabNavigator
+              renderIcon={() => <Ionicons name="ios-apps" size={28} style={{color:'#000'}} />}
+            />
+
+            <TabNavigator
+              renderIcon={() => <Ionicons name="ios-paper" size={28} style={{color:'#000'}} />}
+            >
+            </TabNavigator>
+            <TabNavigator
+              renderIcon={() => <Ionicons name="ios-document" size={28} style={{color:'#000'}} />}
+            >
+            </TabNavigator>
+            <TabNavigator
+              renderIcon={() => <Ionicons name="ios-mail" size={28} style={{color:'#000'}} />}
+            />
+
+            <TabNavigator
+              renderIcon={() => <Ionicons name="ios-people" size={28} style={{color:'#000'}} />}
+            >
+            </TabNavigator>
+            <TabNavigator
+              renderIcon={() => <Ionicons name="ios-share-alt" size={28} style={{color:'#000'}} />}
+            >
+            </TabNavigator>
+        </TabNavigator>
         </View>
-      </ScrollView>
+
+
     );
   }
 }
--- styles.js
+++ styles.js
@@ -30,10 +30,45 @@ export default {
     marginHorizontal: 5,
     marginTop: 3
   },
+  balanceHistory: {
+    flex: 1,
+    padding: 15,
+    width: deviceWidth / 1.04,
+    height: deviceHeight / 2.5,
+    backgroundColor: '#ffffff',
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#ffffff',
+    shadowColor: '#30C1DD',
+    shadowRadius: 10,
+    shadowOpacity: 0.9,
+    elevation: 8,
+    shadowOffset: {width: 0,height: 10},
+    marginHorizontal: 10,
+    marginTop: 50
+  },
+  portfolioDistribution: {
+    flex: 1,
+    padding: 15,
+    width: deviceWidth / 1.04,
+    height: deviceHeight / 1.8,
+    backgroundColor: '#ffffff',
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#ffffff',
+    shadowColor: '#30C1DD',
+    shadowRadius: 10,
+    shadowOpacity: 0.9,
+    elevation: 8,
+    shadowOffset: {width: 0,height: 10},
+    marginHorizontal: 10,
+    marginTop: 50
+  },
   portBalanceStyle: {
     flex: 1,
-    width: deviceWidth / 1.04,
-    height: deviceHeight / 9,
+    padding: 10,
+    width: deviceWidth / 1.06,
+    height: deviceHeight / 6,
     backgroundColor: '#ffffff',
     borderWidth: 1,
     borderRadius: 10,
@@ -49,7 +84,7 @@ export default {
   buttonGradient: {
     flex: 1,
     width: deviceWidth / 2.09,
-    height: deviceHeight / 9,
+    height: deviceHeight /9.1,
     backgroundColor: '#ffffff',
     borderRadius: 10,
     shadowColor: '#30C1DD',
@@ -60,10 +95,24 @@ export default {
     marginHorizontal: 5,
     marginTop: 8
   },
+  buttonGradientBalance: {
+    flex: 1,
+    width: deviceWidth / 2.09,
+    height: deviceHeight / 15,
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    shadowColor: '#30C1DD',
+    shadowRadius: 10,
+    shadowOpacity: 0.9,
+    elevation: 8,
+    shadowOffset: {width: 0,height: 10},
+    marginHorizontal: 5,
+    justifyContent: 'center'
+  },
   addWalletbtn: {
     flex: 1,
     width: deviceWidth / 2.09,
-    height: deviceHeight / 9,
+    height: deviceHeight / 9.1,
     backgroundColor: '#ffffff',
     borderWidth: 1,
     borderRadius: 10,
@@ -74,57 +123,110 @@ export default {
     elevation: 8,
     shadowOffset: {width: 0,height: 10},
     marginHorizontal: 5,
-    marginTop: 5
-  },
-  withdrawBtn: {
-    flex: 1,
-    width: deviceWidth / 2.09,
-    height: deviceHeight / 12,
+    marginTop: 8,
     justifyContent: 'center',
+  },
+  addWallet2nbtn: {
+    flex: 1,
+    backgroundColor: '#ffffff',
     borderRadius: 10,
-    borderColor: '#00E0C1',
     shadowColor: '#30C1DD',
     shadowRadius: 10,
     shadowOpacity: 0.9,
     elevation: 8,
     shadowOffset: {width: 0,height: 10},
     marginHorizontal: 5,
-    marginTop: 5
+    marginTop: 8
   },
   searchInputText: {
     padding: 10,
     width: deviceWidth / 1.5,
     backgroundColor: '#eeeeee',
     borderRadius: 30,
-    marginLeft: 8
+    marginLeft: 8,
   },
   profileImage: {
     width: 50,
     height: 50,
     borderRadius: 50,
-    marginHorizontal: 10
+    marginHorizontal: 10,
+  },
+  profileSentMoneyImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    marginHorizontal: 10,
+  },
+  sentMoneyToView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  addSentMoneyPerson: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    paddingRight: 10
+  },
+  transactionPortImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  sentMoneyImages: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    marginLeft: 50
   },
   commonBtnStyle: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: 7
+    paddingTop: 10,
+    paddingLeft: 10
+  },
+  depositeBtnStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItem: 'center',
   },
   coinDownName: {
     color: '#000',
   },
-  commonBitcoin: {
+  dipositeMoney: {
+    fontSize: 17,
     color: '#44DA79'
   },
+  withdraw: {
+    fontSize: 17,
+    color: '#ffffff',
+    padding: 13
+  },
+  yourPortBalanceBtnStyle: {
+    color: '#90A1B7'
+  },
+  showBalanceHistoryStyle: {
+    color: '#000'
+  },
   commonTextColor: {
-    color: '#ffffff'
+    color: '#ffffff',
+  },
+  commonPortTextColor: {
+    color: '#ffffff',
+    marginTop: 4
   },
   ioniconsDown: {
     marginRight: 10,
-    color: '#FA5D31'
+    color: '#FA5D31',
+    marginTop: 2
   },
   ioniconsUp: {
     marginRight: 10,
-    color: '#44DA79'
+    color: '#44DA79',
+    marginTop: 2
   },
   portStyle: {
     marginRight: 10,
@@ -134,25 +236,58 @@ export default {
     fontSize: 20,
     color: '#FA5D31'
   },
+  portBalance: {
+    fontSize: 35,
+    color: '#000'
+  },
   bitCoinPercentage: {
     fontSize: 20,
     color: '#44DA79'
   },
-  portPercentage: {
+  commonBitcoin: {
+    color: '#44DA79',
+    marginTop: 2
+  },
+  balancePercentage: {
     fontSize: 20,
+    color: '#44DA79',
+    marginTop: 10
+  },
+  portBalanceUpArrow: {
+    color: '#44DA79',
+    marginTop: 10
+  },
+  portPercentage: {
+    fontSize: 22,
     color: '#ffffff'
   },
   commonIcon: {
     flexDirection: 'row',
-    padding: 7
+    paddingBottom: 10,
+    paddingLeft: 10
+  },
+  portBlancePercentageIcon: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 7,
+  },
+  balanceHisrtoryMonthStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 7,
   },
   removeIconColor: {
-    color: '#FA5D31'
+    color: '#FA5D31',
+    marginTop: 6,
+  },
+  dollarIconColor: {
+    color: '#000',
+    marginTop: 20,
   },
   searchBarRow: {
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItem: 'center'
+    alignItem: 'center',
   },
   notificationIcon: {
     marginRight: 10
@@ -163,6 +298,60 @@ export default {
     color:'#00E0C1'
   },
   addWalletTextColor: {
-    color: '#00E0C1'
+    color: '#00E0C1',
+  },
+  portfolioDisatribution: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    padding: 10
+  },
+  bitcoinsBTC: {
+    flexDirection: 'row'
+  },
+  transactionView: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  bitcoinBTCbtn: {
+    flex: 1,
+    width: deviceWidth / 2.09,
+    height: deviceHeight / 8.3,
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    justifyContent: 'center',
+    shadowColor: '#30C1DD',
+    shadowRadius: 10,
+    shadowOpacity: 0.9,
+    elevation: 8,
+    shadowOffset: {width: 0,height: 10},
+    marginHorizontal: 5,
+    marginTop: 8
+  },
+  transactionsStyle: {
+    width: deviceWidth / 2.09,
+    height: deviceHeight / 4.5,
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    padding: 10,
+    shadowColor: '#30C1DD',
+    shadowRadius: 10,
+    shadowOpacity: 0.9,
+    elevation: 8,
+    shadowOffset: {width: 0,height: 10},
+    marginHorizontal: 5,
+    marginTop: 8
+  },
+  transactionMainName: {
+    marginTop: 12,
+    fontSize: 17
+  },
+  transactionCatName: {
+    color: '#ACAFB7',
+    marginTop: 10,
+    fontSize: 15
+  },
+  coinNameColor: {
+    color: '#90A1B7'
   }
 };
